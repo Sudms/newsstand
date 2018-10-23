b3630d046090e4ebcdab7a15643b08258ae96087
SusyBalboa <susanadias@ua.pt>
2018-10-23 20:15:49 +0100

    read from xml

--- app/views.py
+++ app/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from lxml import etree 
+import xmltodict
 
 # Create your views here.
+def newslist(request):
+    
+    file = 'app/data/test2.xml'
+    tree = etree.parse(file)
+        
+    new = tree.xpath('.//title/text()')
+    print(new)
+
+    tparams = {
+        "TITULO " : new
+    }
+
+    return render(request, 'new.html', tparams)      
