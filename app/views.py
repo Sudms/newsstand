f27571b3980c0e1497cf83dbae40290936a28a5f
João Pedro <jpbjesus@ua.pt>
2018-11-01 11:37:37 +0000

    minor fixes

--- app/views.py
+++ app/views.py
@@ -1,24 +1,56 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from lxml import etree, html
+
+import xmltodict
 import requests
+from BaseXClient import BaseXClient
+
+NAMESPACES = {  'dc'        : 'https://purl.org/dc/elements/1.1/', 
+                'itunes'    : 'https://www.itunes.com/dtds/podcast-1.0.dtd'}
+
+# create basex session
+session = BaseXClient.Session('localhost', 1984, 'admin', 'admin')
+
+def getrss(url):
+    tree = html.fromstring(requests.get(url).content)
+    rss = tree.xpath('//link[@type="application/rss+xml"]/@href')
+    return rss
 
 # Create your views here.
 def home(request):
     file = 'app/data/rss2.xml'
     tree = etree.parse(file)
     query = tree.xpath('./channel/item')
+    query2 = tree.xpath('./channel')
     items = []
     
+    for q in query2:
+        feed_title, feed_link = q.find('title').text, q.find('link').text
+    
+    print(feed_title)
+    print(feed_link)
+
     for c in query:
         desc = c.find('description').text.replace('<img src="', "")
         ind = desc.find('" />')
         
         date = c.find('pubDate').text
         img, desc = desc[:ind], desc[ind+4:]
+        comments = c.find('comments').text
+        creator = c.find('dc:creator', NAMESPACES).text
        
-        items.append({'title' : c.find('title').text, 'link' : c.find('link').text, 'description': desc, 'image' : img, 'date': date })
-        print('SUSANA LINDA O QUE PROCURAS ESTÁ AQUI ->', img)
+        items.append({  'feed_title'    : feed_title,
+                        'feed_link'     : feed_link,                   
+                        'title'         : c.find('title').text, 
+                        'link'          : c.find('link').text, 
+                        'description'   : desc, 
+                        'image'         : img, 
+                        'date'          : date,
+                        'creator'       : creator,
+                        'comments'      : comments
+                    })
+
         tparams = {
             "items": list(items)
         }
